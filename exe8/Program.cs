9831e3332e60db8d0c957e9f6013a82cc5fbd753
«Elena <leseee4ka@bk.ru>
2023-07-22 15:40:28 +0300

    exe8

---
+++ exe8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine() ?? "";
+int N = int.Parse(number);
+int index = 2;
+
+while(index <= N)
+{
+    Console.Write($"{index} ");
+    index = index + 2;
+}
+
